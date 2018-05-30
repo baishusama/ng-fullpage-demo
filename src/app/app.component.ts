41c640a7a2220c3a5c04c41aea7e907be888e2d6
baishusama <baishusama@outlook.com>
2018-05-30 08:19:59 +0800

    Initialize components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+/**
+ * How to import jQuery in Angular ?
+ * More on https://stackoverflow.com/questions/30623825/how-to-use-jquery-with-angular#answer-40355691
+ */
+// import { $ } from 'jquery'; // ES6
+import * as $ from 'jquery';
+// declare let $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  @ViewChild('fullpage') fullpage;
+
+  ngOnInit() {
+    console.log('[test] $ :', $);
+    console.log('[test] fullpage :', this.fullpage);
+    $(this.fullpage.nativeElement).fullpage();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FinComponent } from './fin/fin.component';
+import { SliderComponent } from './slider/slider.component';
+import { IntroductionComponent } from './introduction/introduction.component';
 
+import { DataService } from './data.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FinComponent,
+    SliderComponent,
+    IntroductionComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
